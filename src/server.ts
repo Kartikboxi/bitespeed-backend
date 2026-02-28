dd4131220b0cb0b4301ac12265c6d7c02c47bb4d
Kartikboxi <rtakartik@gmail.com>
2026-02-28 07:11:50 +0530

    Update:use of dynamic PORT for deployment

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ console.log("SERVER FILE LOADED");
 
 import app from "./app";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`[SERVER] Running on http://localhost:${PORT}`);
